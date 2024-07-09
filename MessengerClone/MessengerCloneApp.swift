0488ea8ba78861e5918275a72476d89c8889237a
sstonn <sstonn2501@gmail.com>
2024-07-09 14:01:39 +0700

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by sstonn on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
